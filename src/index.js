df0831f0a2ec84dce8ad11ea736be94248571450
Demetria Gutierrez <accounts@calyx.studio>
2020-05-12 11:53:53 -0600

    Added splash image as background

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 import img from './assets/img/sometimes_800.jpg';
-import img from './assets/img/usrsb_stackphotography.png';
+import rangeIcon from './assets/img/usrsb_stackphotography.png';
+
+var rangeImg = document.getElementById('range');
+rangeImg.src = rangeIcon;
 
 function component() {
   const element = document.createElement('div');
