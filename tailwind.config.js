1c8fc9daf84b7d88ba976231dc9718166c7f7116
Demetria Gutierrez <accounts@calyx.studio>
2020-05-17 10:08:02 -0600

    Testing side menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
 			inner: 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
 			outline: '0 0 0 3px rgba(96,171,210,0.5)',
 			focus: '0 0 0 3px rgba(96,171,210,0.5)',
+			menu:'16px 0 16px -2px  rgba(0, 0, 0, .06), 6px 0 16px -2px  rgba(0, 0, 0, .06)',
 			'none': 'none',
 		},
 		extend: {
