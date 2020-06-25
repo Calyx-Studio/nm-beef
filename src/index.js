93e1371b2e1ba40ffe97f0562b52b95fcf1f6ab4
Demetria Gutierrez <accounts@calyx.studio>
2020-06-25 16:58:24 -0600

    Menu-  toggle-able primary + secondary menu ready for review

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash';
 import img from './assets/img/sometimes_800.jpg';
-import rangeIcon from './assets/img/usrsb_stackphotography.png';
+import logoImage from './assets/img/NewMexicoBeefheaderimage091619Final.png';
+import rangeImage from './assets/img/usrsb_stackphotography.png';
+
 
 var rangeImg = document.getElementById('range');
-rangeImg.src = rangeIcon;
+rangeImg.src = rangeImage;
+
+var logoImg = document.getElementById('logo');
+logoImg.src = logoImage;
 
 function component() {
   const element = document.createElement('div');
